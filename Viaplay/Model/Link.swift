5a69d3a362e6d764edb460a5e7ce69a8052496fd
Sara Gharib <sara.gharib91@gmail.com>
2020-02-10 15:06:48 +0100

    [NO-TASK] FEAT - Created Link class

---
+++ Viaplay/Model/Link.swift
@@ -0,0 +1,31 @@
+//
+//  Link.swift
+//  Viaplay
+//
+//  Created by Sara Gharib on 2/10/20.
+//  Copyright © 2020 Sara Gharib. All rights reserved.
+//
+
+import Foundation
+
+struct Link: Decodable {
+    
+    // MARK: - Properties
+    
+    let sections: [Section]?
+    
+    // MARK: - CodingKeys
+    
+    enum CodingKeys: String, CodingKey {
+        case section = "viaplay:sections"
+    }
+    
+    // MARK: - Init
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        sections = try container.decode([Section].self, forKey: .section)
+    }
+}
