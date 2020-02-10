16b38eee32d8bca373c9e7517e72a207ef904ca6
Sara Gharib <sara.gharib91@gmail.com>
2020-02-10 15:02:07 +0100

    [NO-TASK] FEAT - Created Section class

---
+++ Viaplay/Model/Section.swift
@@ -0,0 +1,34 @@
+//
+//  Section.swift
+//  Viaplay
+//
+//  Created by Sara Gharib on 2/10/20.
+//  Copyright © 2020 Sara Gharib. All rights reserved.
+//
+
+import Foundation
+
+struct Section: Decodable {
+    
+    // MARK: - Properties
+    
+    let title: String?
+    let urlString: String?
+    
+    // MARK: - CodingKeys
+    
+    enum CodingKeys: String, CodingKey {
+        case urlString = "href"
+        case title
+    }
+    
+    // MARK: - Init
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        title = try container.decode(String.self, forKey: .title)
+        urlString = try container.decode(String.self, forKey: .urlString)
+    }
+}
